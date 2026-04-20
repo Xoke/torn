b1267798cdbff3c4524416454024e3efd350e537
Xoke <xokesoru@gmail.com>
2026-04-19 17:08:50 -0700

    feat: add TornPropertyWatcher meta.js

---
+++ TornPropertyWatcher.meta.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Torn Property Watcher
+// @namespace    https://xoke.org/
+// @version      1.0
+// @description  Alerts when a cheap Private Island appears on the Torn property market
+// @author       Xoke
+// @match        https://www.torn.com/*
+// @run-at       document-end
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_addStyle
+// @grant        GM_xmlhttpRequest
+// @connect      api.torn.com
+// @homepageURL  https://github.com/Xoke/torn
+// @downloadURL  https://raw.githubusercontent.com/Xoke/torn/main/TornPropertyWatcher.user.js
+// @updateURL    https://raw.githubusercontent.com/Xoke/torn/main/TornPropertyWatcher.meta.js
+// ==/UserScript==
